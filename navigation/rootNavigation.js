d7baf70751d0c2f187cd93b12130e2d44f74bb31
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-08 05:31:04 -0300

    05-Creating an Auth Stack
    05-Creating an Auth Stack

--- navigation/rootNavigation.js
+++ navigation/rootNavigation.js
@@ -1,10 +1,41 @@
+import { useContext, useEffect, useState } from "react"
+import { ActivityIndicator, Text, View } from "react-native"
+import { AppContext } from "../store/appContext";
 
-import { Text } from "react-native"
+/// NAVIGATION
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthStack } from "./navigation";
+
+
+// FIREBASE 
+import { onAuthStateChanged } from "firebase/auth";
+import { AUTH } from "../config/firebase";
+
+
 
 export default function RootNavigator(){
+    const [ loading, setLoading ] = useState(true);
+    const { user, setUser} = useContext(AppContext)
+
+    useEffect(()=>{
+        onAuthStateChanged(AUTH,async(user)=>{
+            user ? setUser(user) : setUser(null);
+            setLoading(false)
+        })
+    },[user]);
+
+
+    if(loading){
+        return(
+            <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+                <ActivityIndicator size="large"/>
+            </View>
+        )
+    }
+
     return(
-        <Text>
-            Hello
-        </Text>
+        <NavigationContainer>
+            { user ? <Text> NORMAL STACK </Text> : <AuthStack/>}
+        </NavigationContainer>
     )
 }
