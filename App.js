fd28feffcdba61136652149ac305cabe04845268
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-08 04:55:33 -0300

    03-Installing Fonts
    03-Installing Fonts

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+/// FONTS
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [loaded,error] = useFonts({
+    'Pacifico':require('./assets/fonts/Pacifico.ttf'),
+    'Anton':require('./assets/fonts/Anton.ttf'),
+  });
+
+  useEffect(()=>{
+    if(loaded || error){
+      SplashScreen.hideAsync();
+    }
+  },[loaded,error])
+  if(!loaded && !error) { return null}
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>here</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
