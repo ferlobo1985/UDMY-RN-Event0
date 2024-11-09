255c235d9951116ec2be67a2b0ef5547077544df
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-09 02:36:28 -0300

    17-Custom input
    17-Custom input

--- components/events/createForm.js
+++ components/events/createForm.js
@@ -1,7 +1,8 @@
-import { Formik } from "formik";
-import { KeyboardAvoidingView, Platform, Text, ScrollView, View} from "react-native";
+import { Formik, Field } from "formik";
+import { KeyboardAvoidingView, Platform, ScrollView, View} from "react-native";
 
 import { eventValidationSchema } from "./event.schema";
+import CustomInput from "../utils/input.cutom";
 
 export default function CreateEventForm(){
 
@@ -26,7 +27,23 @@ export default function CreateEventForm(){
             {({ handleSubmit, isValid })=>(
             <View style={{marginHorizontal:30,marginBottom:50,marginTop:20}}>
 
-                    
+                <Field
+                    component={CustomInput}
+                    name="name"
+                    placeholder="Name of the event"
+                    autoCapitalize={true}
+                />
+
+                <Field
+                    component={CustomInput}
+                    name="description"
+                    placeholder="Enter a description"
+                    autoCapitalize={true}
+                    multiline={true}
+                    numberOfLines={5}
+                />
+
+
 
             </View>
             )}
