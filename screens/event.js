22b2f5fbf2925b69fcec38b5a268eed0fd889795
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-10 00:52:02 -0300

    27-Finishing the event page
    27-Finishing the event page

--- screens/event.js
+++ screens/event.js
@@ -2,6 +2,11 @@ import { useIsFocused } from "@react-navigation/native";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import { getEventById } from "../config/events.firebase";
+import TagComp from "../components/utils/tag";
+import { AppStyle } from "../constants";
+
+import ButtonCustom from '../components/utils/button.custom';
+
 
 export default function Event(props){
     const [ loading,setLoading ] = useState(true);
@@ -25,7 +30,34 @@ export default function Event(props){
             { loading && <ActivityIndicator size="large"/>}
             { !loading && event && 
             <>
-                <Text>{event.name}</Text>
+                {/* HEADER */}
+                <View style={styles.contentHeader}>
+                    <Text>{event.date}, {event.time}</Text>
+                    <TagComp
+                        priority={event.priority}
+                    />
+                </View>
+
+                {/* BODY */}
+                <View>
+                    <Text style={styles.contentTitle}>
+                        {event.name}
+                    </Text>
+                    <Text style={{fontSize:20}}>
+                        {event.description}
+                    </Text>
+                </View>
+
+
+                 {/* FOOTER */}
+                 <View style={styles.contentFooter}>
+                    <ButtonCustom
+                        title="Edit event"
+                        onPress={()=> props.navigation.navigate('Edit Event',{
+                            event: { id: eventID,...event}
+                        }) }
+                    />
+                 </View>
             </>
             }
 
@@ -38,5 +70,23 @@ const styles = StyleSheet.create({
         flex:1,
         backgroundColor:'#fff',
         padding:20
+    },
+    contentHeader:{
+        paddingVertical:10,
+        borderBottomColor:AppStyle.purpLight,
+        borderBottomWidth:1,
+        flexDirection:'row',
+        justifyContent:'space-between',
+    },
+    contentTitle:{
+        fontFamily:'Anton',
+        fontSize:30,
+        marginVertical:20
+    },
+    contentFooter:{
+        marginVertical:20,
+        paddingVertical:20,
+        borderTopColor:AppStyle.purpLight,
+        borderTopWidth:1
     }
 })
