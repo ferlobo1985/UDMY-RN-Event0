7ad20fe2d82656ab29388005e3fcd860bd35b3d1
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-09 23:40:40 -0300

    24-Refresh events
    24-Refresh events

--- screens/home.js
+++ screens/home.js
@@ -1,6 +1,6 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import AddEventButton from "../components/utils/addEventButton";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../store/appContext";
 
 import EventCard from '../components/utils/event.card'
@@ -8,6 +8,7 @@ import { useNavigation } from "@react-navigation/native";
 
 export default function Home(){
     const context = useContext(AppContext);
+    const [refreshing,setRefreshing] = useState(false);
     const navigation = useNavigation();
 
 
@@ -22,12 +23,21 @@ export default function Home(){
         })
     }
 
+    const onRefresh = async()=>{
+        setRefreshing(true)
+        await context.getHomeEvents().then(()=>{
+            setRefreshing(false)
+        });
+    }
+
 
     return(
         <View style={styles.container}>
             { context.eventState.events &&
                 <FlatList
                     data={context.eventState.events}
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
                     renderItem={({item})=>(
                         <EventCard
                             eventPressHandler={()=> eventPressHandler(item)}
